10395bf3aa7efa0d5345d91f8893e0a6fa2138a2
Sasha <hfgghf@gmail.com>
2022-04-26 18:28:33 +0500

    операции с массивами

--- js/17object.js
+++ js/17object.js
@@ -11,14 +11,17 @@ const options = {
 };
 console.log(options.name);
 //delete options.name;
-console.log(options);
+console.log(options); // выводит массив
+console.log(Object.keys(options).length); // выводит объекты массива
+console.log(Object.keys(options).length); //4 выводит кол-во объектов
 
-let counter = 0;
+
+/* let counter = 0;
 for (let key in options) {
     if (typeof(options[key]) === 'object') {
         for (let i in options[key]) {
         console.log(`Свойства ${i} имеет значение ${options[key][i]}`);
-        counter++;
+        //counter++;
         }
     } else {
         console.log(`Свойства ${key} имеет значение ${options[key]}`);
@@ -26,6 +29,48 @@ for (let key in options) {
     }
 
 }
-console.log(counter);
+console.log(counter); */
 
 
+// Тестовый объект (для запоминания)
+
+const miniBD = {
+    type: 'short',
+    print: 'hanna',
+    kolvo: 78,
+    tag: 'hanna_m',
+    uri: 'h78',
+    color: {
+        line: 'red',
+        topii: 'blue',
+        general: 'red',
+    },
+    makeTest: function() {
+        console.log("Test");
+    },
+};
+miniBD.makeTest();
+const {line, topii} = miniBD.color;
+console.log(line);
+
+console.log(Object.keys(miniBD));
+console.log(Object.keys(miniBD).length);
+console.log(Object.values(miniBD));
+console.log(Object.entries(miniBD));
+//console.log(miniBD);
+//console.log(miniBD.print);
+/* for (let key in miniBD) {
+    //console.log(miniBD[key]);
+    console.log(`Свойство ${key} иммеет значение ${miniBD[key]}`);
+} */
+
+for (let key in miniBD) {
+    if (typeof(miniBD[key]) === 'object') {
+        for (let i in miniBD[key]) {
+            console.log(`Свойство ${i} иммеет значение ${miniBD[key][i]}`);
+        }}
+        else {
+            console.log(`Свойство ${key} иммеет значение ${miniBD[key]}`);
+        }
+    }
+   
--- js/callback_funkcii.js
+++ js/callback_funkcii.js
@@ -15,10 +15,14 @@ function second() {
 first();
 second();
 
-function learnJS(lang, callback) {
+
+
+
+function learnJS(lang, callback) { //объявляем функцию с 2 аргументами. 2 арг - функция
     console.log(`Я учю: ${lang}`);
-    callback();
+    callback();                     //выполняем 2 функцию только после console.log
 }
-learnJS('Javascript', function() {
-    console.log('Я прошёл этот урок!');
-})
+
+learnJS('Javascript', function() {      //запускаем функцию с 2 аргументами
+    console.log('Я прошёл этот урок!'); //2 аргумент - функция с текстом
+});
