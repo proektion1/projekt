02c4f19d1239fa8dd1979f0a5453ee8bdf2328ef
Sasha <hfgghf@gmail.com>
2022-04-26 14:06:14 +0500

    опереции с объектами

---
+++ js/17object.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red',
+    }
+};
+console.log(options.name);
+//delete options.name;
+console.log(options);
+
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+        console.log(`Свойства ${i} имеет значение ${options[key][i]}`);
+        counter++;
+        }
+    } else {
+        console.log(`Свойства ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+
+}
+console.log(counter);
+
+
