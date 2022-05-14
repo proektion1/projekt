1a8d747a21ddd8489ebe109a488a4d7221fd2cb2
Sasha <hfgghf@gmail.com>
2022-05-14 17:06:32 +0500

    Ловим ошибки в консоли

---
+++ js/22lovim_error.js
@@ -0,0 +1,19 @@
+"use stict";
+
+function hello() {
+    console.log("hello, world!");
+}
+hello();
+
+function hi() {
+    console.log("Say hi");
+}
+hi();
+
+const arr = [1, 14, 4, 30, 54],
+sorted = arr.sort(compareNum);
+
+function compareNum(a, b) {
+        return a - b;
+}
+console.log(sorted);
