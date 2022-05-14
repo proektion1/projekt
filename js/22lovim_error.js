a5a3024331c5f8c489ad6987e1727fffebe63d52
Sasha <hfgghf@gmail.com>
2022-05-14 17:10:23 +0500

    Ловим ошибки в консолb

--- js/22lovim_error.js
+++ js/22lovim_error.js
@@ -10,8 +10,9 @@ function hi() {
 }
 hi();
 
-const arr = [1, 14, 4, 30, 54],
-sorted = arr.sort(compareNum);
+const arr = [1, 14, 4, 30, 54];
+
+const sorted = arr.sort(compareNum);
 
 function compareNum(a, b) {
         return a - b;
