24ef1ad5677d8eaf4b13cf0870c133684ff9dc6d
Sasha <hfgghf@gmail.com>
2022-05-25 23:19:32 +0500

    Date

---
+++ js_step_2/5 Data/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const now = new Date(); //текущая дата
+console.log(now);
+
+const now1 = new Date();            //текущая дата
+console.log(now1);                  //2022-05-25T17:57:03.499Z
+console.log(now1.getFullYear());    //2022
+console.log(now1.getMonth());       //4
+console.log(now1.getDate());        //25    
+console.log(now1.getDay());         //3 (начин с воскресен)
+console.log(now1.getUTCHours());    //17 часов по UTC
+console.log(now1.getTimezoneOffset()); //-300 отличие зоны
+console.log(now1.getTime()); //сколько милисекунд с 1970 года
+
+console.log(now1.setHours(18)); // устновить время равное 18 часам
+console.log(now1.setHours(18, 40)); // устновить время 18-40 часам
+
+
+//Бенчмарк - производительность системы
+let start = new Date();
+for (let i = 0; i < 100; i++) {
+    let some = i ** 3;
+    console.log(i);
+}
+let end = new Date();
+console.log(`Цикл отработал за ${end - start} миллисекунд`);
