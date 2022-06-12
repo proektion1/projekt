5e72213c3b48d670357e451f0b9fcedf0ed045b7
Sasha <hfgghf@gmail.com>
2022-06-12 15:16:54 +0500

    sam

--- js_step_2/16 PRAKTIKA (restoran)/js/script.js
+++ js_step_2/16 PRAKTIKA (restoran)/js/script.js
@@ -1,137 +1,132 @@
-//==========Сделать табы в меню (Выберите стиль питания)
-//1. В начале убираем весь контент для табов, чтобы оно не налазили друг на друга   
-/* Также убираем активный стиль у табов
-Показываем контент для 1-го таба
-скрываем контент табов и активный стиль у кнопки
-Показываем контент 1 таба (0 индекс псевдомассива) и добавляем активный ксс
-3 Делаем табы по нажатию */
-		 
-		
-//2. Сделать таймер		 
-// TIMER
-//1. Определяем сколько дней, часов, минут до deadline (2022-06-30)
-// Если 5 дней, то выводить 05 дней/ Также с секундами и т.д.
-//ЧТобы не моргала верстка timeInterval = setInterval(updateClock, 1000);
-// 1000 (1 сек) не нужно было ждать. Запустим 1 раз вручную		 
-		 
-/* Задача 9
-Открытие модальных окон на 2 кнопках
-Закрытие модального окна через крестик 
-Закрытие модального окна через кнопку ескейп
+//1. Сделать меню с табами
+//2. Сделать таймер
+//3. Открыть модальные окна и закрыть (закрыть по кнопке, клику вне формы и клавише Esc)
+//4. Автоматическое модальное окно через 3 секунды + при скролле в самый низ
 
-
-Задача 10
-Появление модального окна через 8 секунд
-Если форму юзер получил сам через кнопку - больше не показывать
-Когда долистали до конца - показать форму
-Форма не должна появляться каждый раз при долистывании (только 1 раз) */
 
 const tabContent = document.querySelectorAll('.tabcontent');
-const vseTaby = document.querySelector('.tabheader__items');
-const tab = vseTaby.querySelectorAll('.tabheader__item');
-
+const oblastMenu = document.querySelector('.tabheader__items');
+const menuTab = oblastMenu.querySelectorAll('.tabheader__item');
 
 function hide() {
-	tabContent.forEach(item => {
-		item.classList.add('hide');
-		item.classList.remove('show');
-	});
-	tab.forEach(item => {
-		item.classList.remove('tabheader__item_active');
-	});
+tabContent.forEach(element => {
+    element.classList.add('hide');
+});
+menuTab.forEach(element => {
+    element.classList.remove('tabheader__item_active');
+});
 }
-
-function show(item = 0) {
-	tabContent[item].classList.add('show');
-	tabContent[item].classList.remove('hide');
-	tab[item].classList.add('tabheader__item_active');
+function showMenu (i=0) {
+    tabContent[i].classList.add('show');
+    tabContent[i].classList.remove('hide');
+    menuTab[i].classList.add('tabheader__item_active');
 }
 
 hide();
-show();
+showMenu();
 
-vseTaby.addEventListener('click', (e) => {
-	let target = e.target;
-	if (target && target.classList.contains('tabheader__item')) {
-		tab.forEach((btn, i) => {
-			if (btn == target) {
-				hide();
-				show(i);
-			}
-		});
-	}
+oblastMenu.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target && target.classList.contains('tabheader__item')) {
+        menuTab.forEach((element, i) => {
+            if (element == target) {
+                hide();
+                showMenu(i);
+            }
+        });
+    }
 });
 
-const timer = document.querySelector('.timer');
-const days = timer.querySelector('#days');
-const hours = timer.querySelector('#hours');
-const minutes = timer.querySelector('#minutes');
-const seconds = timer.querySelector('#seconds');
-const deadline = '2022-06-30';
-function opredvremya() {
-	const AllTimeSec = Date.parse(deadline) - Date.parse(new Date());
-	const dni = Math.floor(AllTimeSec / (1000*60*60*24));
-	const hour = Math.floor(AllTimeSec/(1000*60*60) % 24);
-	const minute = Math.floor(AllTimeSec/(1000*60) % 60);
-	const sec = Math.floor(AllTimeSec/(1000) % 60);
-	return {
-		AllTimeSec: AllTimeSec,
-		dni: dni,
-		hour: hour,
-		minute: minute,
-		sec: sec
-	};
+
+const vremyaNaStr = document.querySelector('.timer');
+const days = vremyaNaStr.querySelector('#days');
+const hours = vremyaNaStr.querySelector('#hours');
+const minutes = vremyaNaStr.querySelector('#minutes');
+const seconds = vremyaNaStr.querySelector('#seconds');
+const deadline = '2022-06-27';
+vyvodimVremya();
+
+function smotrimVremya () {
+    const vremya = Date.parse(deadline) - Date.parse(new Date());
+    const day = Math.floor(vremya / (1000*60*60*24));
+    const hour = Math.floor(vremya / (1000*60*60) % 24);
+    const min = Math.floor(vremya /  (1000*60) % 60);
+    const sec = Math.floor((vremya / 1000) % 60);
+    return {
+        vremya: vremya,
+        day: day,
+        hour: hour,
+        min: min,
+        sec: sec
+    };
 }
 
-function zero(x) {
-	if (x>=0 && x< 10) {
-		return `0${x}`;
-	}
-	return x;
-}
-pishemVremya ();
-
-function pishemVremya () {
-	const t = opredvremya(deadline);
-	days.innerHTML = zero(t.dni);
-	hours.innerHTML = zero(t.hour);
-	minutes.innerHTML = zero(t.minute);
-	seconds.innerHTML = zero(t.sec);
-
-	const timeInterval = setInterval(pishemVremya, 1000);
-	if (t.AllTimeSec <= 0) {
-		clearInterval(timeInterval);
-	}
-	
+function getZero (i) {
+    if (i>=0 && i< 10) {
+        return `0${i}`;
+    }
+    return i;
 }
 
-const knopka = document.querySelectorAll('[data-modal]');
-const okno = document.querySelector('.modal');
-const closeKnopka = document.querySelector('.modal__close');
+function vyvodimVremya() {
+    const retVremya = smotrimVremya();
+    days.innerHTML = getZero(retVremya.day);
+    hours.innerHTML = getZero(retVremya.hour);
+    minutes.innerHTML = getZero(retVremya.min);
+    seconds.innerHTML = getZero(retVremya.sec);
+    const x = setInterval(vyvodimVremya, 1000);
+    if (retVremya.vremya <= 0) {
+        clearInterval(vyvodimVremya);
+    }
+}
+vyvodimVremya();
 
-knopka.forEach(element => {
-	element.addEventListener('click', () => {
-		okno.classList.add('show');
-		okno.classList.remove('hide');
-	});
+
+const modal = document.querySelector('.modal');
+const modalClose = document.querySelector('.modal__close');
+const modalBtn = document.querySelectorAll('[data-modal]');
+
+const modalTimerId = setTimeout(showModal, 3000);
+
+
+function showModal () {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId); // Если юзер сам открыл, то через время не открываем
+    window.removeEventListener('scroll', showModalByScroll);
+
+}
+
+function closeModal () {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = "";
+}
+
+modalBtn.forEach(element => {
+    element.addEventListener('click', showModal);
+    
 });
-closeKnopka.addEventListener('click', () => {
-	okno.classList.add('hide');
-	okno.classList.remove('show');
+modalClose.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        closeModal();
+    }
 });
 
 document.addEventListener('keydown', (e) => {
-	if (e.code === "Escape" && okno.classList.contains('show')) {
-		//event code смотреть в интернете
-		// Код работает только когда открыто модальное окно
-		okno.classList.add('hide');
-	okno.classList.remove('show');
-	}
- });
+    if (e.code === "Escape" && modal.classList.contains('show')) {
+        closeModal();
+    }
+});
 
- okno.addEventListener('click', (e) =>{
-	if (e.target === okno) {
-		okno.classList.add('hide');
-		okno.classList.remove('show');
-	}
- });
+
+function showModalByScroll () {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        showModal();
+        window.removeEventListener('scroll', showModalByScroll); // сбросить обработчик, чтобы каждый раз не вызывался при прокрутке вниз
+      }
+}
+window.addEventListener('scroll', showModalByScroll);
